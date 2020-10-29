536df53ee13d8170e09a3371282d334ff566320c
minhtuit <loveae9b.com>
2020-10-30 00:39:38 +0700

    commit

--- src/GitCommand.js
+++ src/GitCommand.js
@@ -4,6 +4,10 @@ import gitCommand from './image/git-command.png';
 function GitCommand(props) {
     return(
         <div id={props.id}>
+            <h1>Thao tác với git</h1>
+            <h2>git add . // Để thêm tất cả các file đã sửa để chuẩn bị commit </h2>
+            <h2>git commit -m "Nội dung commit" //Để commit các file đã được add</h2>
+            <h2>git push -u origin main //Đẩy các file đã commit lên origin main (git hub)</h2>
             <img src={gitCommand} alt="GitCommand" />
         </div>
     );
